b881e5de94d8862803d16b0cdd4ef63f83199ad3
ns1243 <ns1243@hanmail.net>
2017-05-04 05:46:58 +0900

    Add files via upload

---
+++ ch 13 tensorboard/dos_command.py
@@ -0,0 +1,10 @@
+
+
+import os
+
+#os.system('tensorboard --logdir=tensorboard1')
+#os.system('tensorboard --logdir=name_scope1')
+#os.system('tensorboard --logdir=./linear/tensorboard2')
+#os.system('tensorboard --logdir=histogram')
+#os.system('tensorboard --logdir=histogram/ml')
+os.system('tensorboard --logdir=./scalar')
