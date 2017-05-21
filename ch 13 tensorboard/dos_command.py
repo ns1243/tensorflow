041a088e013cdae873d03a5fda96e6a291eaaf7f
ns1243 <ns1243@hanmail.net>
2017-05-22 06:49:52 +0900

    Update dos_command.py

--- ch 13 tensorboard/dos_command.py
+++ ch 13 tensorboard/dos_command.py
@@ -2,9 +2,4 @@
 
 import os
 
-#os.system('tensorboard --logdir=tensorboard1')
-#os.system('tensorboard --logdir=name_scope1')
-#os.system('tensorboard --logdir=./linear/tensorboard2')
-#os.system('tensorboard --logdir=histogram')
-#os.system('tensorboard --logdir=histogram/ml')
-os.system('tensorboard --logdir=./scalar')
+os.system('tensorboard --logdir=./graphs')
